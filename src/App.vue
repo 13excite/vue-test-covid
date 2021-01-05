f5a6d5ae3aa57d487260b0a87574894e0ce73382
Dulenov Vladimir <v.dulenov@corp.vk.com>
2021-01-05 10:40:40 +0300

    Update App.vue. Changed country vue. Add vuex support

--- src/App.vue
+++ src/App.vue
@@ -53,10 +53,9 @@
             <v-icon>mdi-dots-vertical</v-icon>
           </v-btn>
         </template>
-
         <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Country {{ n }}</v-list-item-title>
+          <v-list-item v-for="country in countries" :key="country" @click="() => {}" link :to="'/country/' + country">
+            <v-list-item-title @click="updateCountry(country)">{{ country }}</v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
@@ -87,6 +86,19 @@ export default {
         { title: 'News', icon: 'mdi-newspaper', link: '/' }
       ],
       drawer: false
+    }
+  },
+  mounted () {
+    this.$store.dispatch('updateTopCountries')
+  },
+  methods: {
+    updateCountry (country) {
+      this.$store.dispatch('newCountryName', country)
+    }
+  },
+  computed: {
+    countries () {
+      return this.$store.getters.topCountries
     }
   }
 }
